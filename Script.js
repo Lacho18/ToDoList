53cea1c75765d74b42dffbe047b7c7ba2e490957
Lacho18 <121165891+Lacho18@users.noreply.github.com>
2023-09-01 20:20:11 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -7,7 +7,7 @@ let tasksContext = ["Go to the gym", "Swim", "Go to the store", "Buy food for di
 
 taskDiv.appendChild(list);
 
-function updateList(tasks) {
+function updateList(tasks, boolean) {
     for(let i = 0; i < tasks.length; i++) {
         let task = document.createElement("li");
         task.appendChild(taskText(tasks[i]));
@@ -16,23 +16,45 @@ function updateList(tasks) {
     }
 }
 
+function updateList(tasks, specificTask) {
+    console.log("Tuka sum be");
+    for(let i = 0; i < tasks.length; i++) {
+        let task = document.createElement("li");
+        task.appendChild(taskText(tasks[i]));
+        task.id = tasks[i];
+        list.appendChild(task);
+    }
+
+    IgnoredTaskHandle(specificTask);
+    let interval = setInterval(() => {
+        onGreenButtonClick(specificTask);
+    }, 1000 * 10);
+}
+
 function taskText(text) {
     let greenButton = document.createElement("button");
-    greenButton.innerHTML = "G";
+    let bookmarkImage = document.createElement("img");
+    bookmarkImage.src = "Images/Bookmark.png";
+    greenButton.appendChild(bookmarkImage);
     greenButton.id = text+" CORRECT";
     greenButton.className = "CompleteTaskButton";
+    greenButton.style.backgroundColor = "#66ff33";
     
-    greenButton.style.b
-    /*let redButton = document.createElement("button");
-    redButton.innerHTML = "R";
-    redButton.id = text+" INCORRECT";*/
+    let redButton = document.createElement("button");
+    let XImage = document.createElement("img");
+    XImage.src = "Images/X.png";
+    redButton.appendChild(XImage);
+    redButton.id = text+" INCORRECT";
+    redButton.className = "CompleteTaskButton";
+    redButton.style.backgroundColor = "#e62e00";
 
     greenButton.addEventListener("click", () => {onGreenButtonClick(greenButton.id)});
+    redButton.addEventListener("click", () => {onRedButtonClick(redButton.id);});
 
     let paragraf = document.createElement("p");
     paragraf.innerHTML = text;
     paragraf.appendChild(greenButton);
-    //paragraf.appendChild(redButton);
+    paragraf.appendChild(redButton);
     return paragraf;
 }
 
@@ -44,6 +66,26 @@ function onGreenButtonClick(buttonID) {
             tasksContext.splice(i, 1);
             clearList();
             updateList(tasksContext);
+        }
+    }
+}
+
+function onRedButtonClick(buttonID) {
+    let textValue = buttonID;
+    textValue = textValue.replace(" INCORRECT", "");
+    
+    console.log(textValue);
+
+    for(let i = 0; i < tasksContext.length; i++) {
+        if(textValue === tasksContext[i]) {
+            
+            if(textValue !== tasksContext[tasksContext.length - 1]) {
+                tasksContext = Arrangement(tasksContext, i);
+                
+                clearList();
+                updateList(tasksContext, textValue);
+            }
+            break;
         }
     }
 }
@@ -62,6 +104,38 @@ function checkForRepeat(ulElement, value) {
     }
 
     return true;
+}
+
+function checkForExistance(array, value) {
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === value) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function Arrangement(array, markedIndex) {
+    let j = array[markedIndex];
+    if(j != array.length - 1) {
+        array.push(j);
+        array.splice(markedIndex, 1);
+    }
+
+    return array;
+}
+
+function IgnoredTaskHandle(taskID) {
+    let liElement = document.getElementById(taskID);
+    console.log(liElement)
+    liElement.style.backgroundColor = "rgba(89, 89, 89, 0.5)";
+    liElement.style.borderRadius = "12px";
+    liElement.style.opacity = "0.6";
+    let button1 = document.getElementById(taskID + " CORRECT");
+    let button2 = document.getElementById(taskID + " INCORRECT");
+    button1.remove();
+    button2.remove();
 }
 
 updateList(tasksContext);
